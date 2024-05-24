b7e06bc8980da726779583e9238bffdfa71cf383
Diana <dianalop1998@gmail.com>
2024-05-23 23:36:41 -0400

    Add backend code

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+// Express
+const app = express();
+const PORT = process.env.PORT || 5000; 
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//Endpoints
+app.post('/analyze', async (req, res) => {
+    const { text } = req.body; 
+
+    try {
+        const response = await axios.post('https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english', {
+        input: text
+    }, {
+        headers: {
+            'Authorization': `Bearer ${process.env.HUGGINGFACE_API_KEY}`
+        }
+    });
+
+    res.json(response.data); 
+} catch(error) {
+    res.status(500).send('Error analyzing sentiment');
+
+}
+
+}); 
+
+
+//Server 
+app.listen(PORT, () => {
+    console.log(`Server is running in port ${PORT}`)
+});
